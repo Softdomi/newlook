4cb80c4a2386622ba2c15734a72bb3f273d57f81
Softdomi <142529800+Softdomi@users.noreply.github.com>
2024-07-01 01:46:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const kill = "dont get bosa"
+const isCan = kill == "dont get bosa" 
+
+if ( isCan) {
+    console.log("Mahmoud Can Kill Zeinab")
+}else {
+    console.log("احترمت نفسها وجابت حق ربنا")
+}
